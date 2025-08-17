bc0888415d02de80dc38cc4f8e0b4aaf94de8d50
CHIZI11 <86352547+CHIZI11@users.noreply.github.com>
2025-08-18 02:31:16 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,31 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('app').then(cache => cache.addAll(['/'])));
+// Простой офлайн-кэш для GitHub Pages / статического хостинга
+const CACHE = "cal-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon.svg"
+  // PNG-иконки можно добавить при наличии:
+  // "./icons/icon-192.png",
+  // "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
